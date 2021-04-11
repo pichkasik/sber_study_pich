1b7e1430f8e2c0cd504164ed37fa16793c3b4b13
pichkasik <sasha.marakulin@mail.ru>
2021-04-11 15:02:57 +0300

    Add version 1.0 ZOO

---
+++ src/ru/sbrstudy/homework/homework03/zoo/ZOO.java
@@ -0,0 +1,23 @@
+package ru.sbrstudy.homework.homework03.zoo;
+
+import ru.sbrstudy.homework.homework03.zoo.animal.Animal;
+import ru.sbrstudy.homework.homework03.zoo.animal.Tiger;
+
+public class ZOO {
+	public static void main(String[] args) {
+		System.out.println("=ZOO=");
+		var tiger1 = new Tiger();
+		var tigerJhon = new Tiger("Jhon");
+		tigerJhon.say();
+		tigerJhon.wakeUp();
+		tigerJhon.goSleep();
+		tigerJhon.goSleep();
+		tigerJhon.wakeUp();
+		tiger1.say();
+		tiger1.wakeUp();
+		tiger1.goSleep();
+		tigerJhon.goSleep();
+		tiger1.wakeUp();
+	}
+
+}
