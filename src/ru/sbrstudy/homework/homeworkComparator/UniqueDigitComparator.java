94151e7cb13943f42bae781d10914055d633c8a8
pichkasik <sasha.marakulin@mail.ru>
2021-04-24 18:01:14 +0300

    Fix if comparator return 0 and equals is false

--- src/ru/sbrstudy/homework/homeworkComparator/UniqueDigitComparator.java
+++ src/ru/sbrstudy/homework/homeworkComparator/UniqueDigitComparator.java
@@ -3,7 +3,7 @@ package ru.sbrstudy.homework.homeworkComparator;
 import java.util.Comparator;
 import java.util.HashSet;
 
-public class UniqueDigitCompare implements Comparator<MyInteger> {
+public class UniqueDigitComparator implements Comparator<MyInteger> {
 
 	private int getNumberUniqueDigits(Integer nbr){
 		var digitsHashSet = new HashSet<Integer>();
@@ -19,9 +19,15 @@ public class UniqueDigitCompare implements Comparator<MyInteger> {
 
 	@Override
 	public int compare(MyInteger nbr1, MyInteger nbr2) {
+		int result;
 		Integer numberUniqueDigits1 = getNumberUniqueDigits(nbr1.getValue());
 		Integer numberUniqueDigits2 = getNumberUniqueDigits(nbr2.getValue());
-		//TODO add equals method if return is 0
-		return numberUniqueDigits1.compareTo(numberUniqueDigits2);
+		result = numberUniqueDigits1.compareTo(numberUniqueDigits2);
+		if (result == 0 && !nbr1.equals(nbr2)) {
+			return Integer.compare(nbr1.getValue(), nbr2.getValue());
+		}
+		else {
+			return result;
+		}
 	}
 }
