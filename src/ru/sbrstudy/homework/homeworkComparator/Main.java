a2e4f2056a42cf90ad556c15946899a5278e4989
pichkasik <sasha.marakulin@mail.ru>
2021-04-24 20:07:23 +0300

    Set correct time to wait in message

--- src/ru/sbrstudy/homework/homeworkComparator/Main.java
+++ src/ru/sbrstudy/homework/homeworkComparator/Main.java
@@ -5,9 +5,8 @@ import java.util.TreeSet;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Wait for 10 seconds to see output there is a big integers to work with");
+		System.out.println("Wait for 60 seconds to see output there is a big integers to work with");
 		List<Integer> integerList = List.of(-5, 0, 1, 60, 10000, 367, 24, 2147483647, -2147483648, 1234567890, 123456789, 123456789);
-		//TODO try List.sort or Collection.sort
 		TreeSet<MyInteger> integerTreeSetPrime = new TreeSet<MyInteger>(new PrimeNumberComparator());
 		for (var integer: integerList) {
 			integerTreeSetPrime.add(new MyInteger(integer));
