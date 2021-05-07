dcb3668c7a50c96f416b967b12c23ec5b89cff2d
pichkasik <sasha.marakulin@mail.ru>
2021-05-07 23:07:13 +0300

    Add intersection task

--- src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
+++ src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
@@ -11,8 +11,7 @@ import java.util.stream.Stream;
 
 public class PracticeStream {
 
-	public static void exampleStreamIntegers()
-	{
+	public static void exampleStreamIntegers() {
 		int maxNbr = 1000;
 		int minNbr = -1000;
 		int valueSum = 0;
@@ -28,8 +27,7 @@ public class PracticeStream {
 		hashSet.forEach(System.out::println);
 	}
 
-	public static void exampleListUniqueNames()
-	{
+	public static void exampleListUniqueNames() {
 		//("Jon", "Nick", "Zac");
 		List<String> firstNameList = Arrays.asList("Jon", "Nick", "Zac", "Rick");
 		List<String> secondNameList = Arrays.asList("Jon", "Nate", "Phil", "Lessie");
@@ -42,12 +40,45 @@ public class PracticeStream {
 		System.out.println(uniqueName);
 	}
 
+	public static void exampleAverageWeight() {
+		OptionalDouble averageWeight = OptionalDouble.empty();
+		Map<String, Double> mapWeight = new HashMap<>() {
+			{
+				put("Jon", 50.5d);
+				put("Nick", 70.7d);
+				put("Zac", 150.3d);
+				put("Rick", 63.5d);
+			}
+		};
+		Collection<Double> listWeigth = mapWeight.values();
+		averageWeight = listWeigth.stream().mapToDouble(Double::doubleValue).average();
+		if (averageWeight.isPresent()){
+			System.out.println("Average weight: " + averageWeight.getAsDouble());
+		}
+		else{
+			System.out.println("There is no average Weight");
+		}
+	}
+
+	public static void exampleIntersection(){
+		List<Long> firstList = Arrays.asList(3L, 4L, 5L, 6L, 0L, 0L, -1L, -5L, 9223372036854775807L, -9223372036854775808L);
+		List<Long> secondList = Arrays.asList(33L, 44L, 55L, 6L, 0L, -11L, -5L, 9223372036854775807L, -92233720L);
+		List<Long> intersectionList = firstList.stream()
+										.distinct()
+										.filter(secondList::contains)
+										.collect(Collectors.toList());
+		System.out.println(intersectionList);
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("======================\n==Stream of integers==\n======================");
 		exampleStreamIntegers();
 		System.out.println("\n======================\n==List of Unique Names==\n======================");
 		exampleListUniqueNames();
-
+		System.out.println("\n======================\n==Average Weight==\n======================");
+		exampleAverageWeight();
+		System.out.println("\n======================\n==Intersection==\n======================");
+		exampleIntersection();
 	}
 }
