40b699b53467bd7d4826246ee193f885a3fc9eff
pichkasik <sasha.marakulin@mail.ru>
2021-05-07 21:55:13 +0300

    Make a ListUniqueNames. Refactor StreamIntegers

--- src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
+++ src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
@@ -2,10 +2,8 @@ package ru.sbrstudy.homework.homeworkStream;
 
 import org.w3c.dom.ls.LSOutput;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -23,17 +21,33 @@ public class PracticeStream {
 		randomInts.filter(integer -> integer > 0 && integer % 2 != 0)
 				.map(integer -> integer * 2)
 				.limit(15)
-				.forEach(x -> hashSet.add(x));
+				.forEach(hashSet::add);
 		valueSum = hashSet.stream().reduce(0, Integer::sum);
 		System.out.println("The sum of hashSet = " + valueSum );
 		System.out.println("The hashSet:");
 		hashSet.forEach(System.out::println);
 	}
 
+	public static void exampleListUniqueNames()
+	{
+		//("Jon", "Nick", "Zac");
+		List<String> firstNameList = Arrays.asList("Jon", "Nick", "Zac", "Rick");
+		List<String> secondNameList = Arrays.asList("Jon", "Nate", "Phil", "Lessie");
+		List<String> thirdNameList = Arrays.asList("Ilya", "Nick", "Rob", "Fred");
+		List<String> fourthNameList = Arrays.asList("Nancy", "Stephanie", "Zac", "Madison");
+		Stream<String> combineStreams = Stream.of(firstNameList, secondNameList, thirdNameList, fourthNameList)
+												.flatMap(Collection::stream)
+												.distinct();
+		ArrayList<String> uniqueName = combineStreams.collect(Collectors.toCollection(ArrayList::new));
+		System.out.println(uniqueName);
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("======================\n==Stream of integers==\n======================");
 		exampleStreamIntegers();
-		System.out.println("\n======================\n==Stream of string==\n======================");
+		System.out.println("\n======================\n==List of Unique Names==\n======================");
+		exampleListUniqueNames();
+
 	}
 }
