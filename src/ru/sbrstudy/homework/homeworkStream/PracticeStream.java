7f7548695ce917477778996a8752c2ccc9d2a9ec
pichkasik <sasha.marakulin@mail.ru>
2021-05-07 21:30:37 +0300

    Make task with Stream of integers

--- src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
+++ src/ru/sbrstudy/homework/homeworkStream/PracticeStream.java
@@ -14,18 +14,19 @@ import java.util.stream.Stream;
 public class PracticeStream {
 	public static void main(String[] args) {
 
-		int maxNbr = 2147483647 / 2;
-		int minNbr = -2147483648;
-		long valueSum = 0;
+		int maxNbr = 1000;
+		int minNbr = -1000;
+		int valueSum = 0;
 		IntStream randomInts = new Random().ints(minNbr, maxNbr);
 		Set<Integer> hashSet = new HashSet<>();
 		randomInts.filter(integer -> integer > 0 && integer % 2 != 0)
 					.map(integer -> integer * 2)
 					.limit(15)
 					.forEach(x -> hashSet.add(x));
-		valueSum = hashSet.stream().reduce(0, (x, y) -> x + y);
-		System.out.println("The sum(could be negative if long overflowed) of hashSet = " + valueSum );
+		valueSum = hashSet.stream().reduce(0, Integer::sum);
+		System.out.println("======================\n==Stream of integers==\n======================");
+		System.out.println("The sum of hashSet = " + valueSum );
 		System.out.println("The hashSet:");
-		hashSet.stream().forEach(System.out::println);
+		hashSet.forEach(System.out::println);
 	}
 }
